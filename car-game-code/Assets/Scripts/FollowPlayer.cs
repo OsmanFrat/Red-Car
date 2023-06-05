9d648f1f94944e1acd90e8c1762362b443ab47bb
Osman Fırat <osmanfrat@yahoo.com>
2023-06-05 14:38:51 +0300

    optimization

--- car-game-code/Assets/Scripts/FollowPlayer.cs
+++ car-game-code/Assets/Scripts/FollowPlayer.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset1 = new Vector3(0, 5, -7);
-    private Vector3 offset2 = new Vector3(0,3,1);
-    private Vector3 offset3 = new Vector3(10,6,0);
+    [SerializeField] readonly Vector3 offset1 = new Vector3(0, 5, -7);
+    [SerializeField] readonly Vector3 offset2 = new Vector3(0,3,1);
+    [SerializeField] readonly Vector3 offset3 = new Vector3(10,6,0);
 
 
     void LateUpdate()
     {
         transform.position = player.transform.position + offset1;
-        
-  
-
     }
 
 }
--- car-game-code/Assets/Scripts/PlayerController.cs
+++ car-game-code/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,10 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     // Private Variables
-    private float speed = 20.0f;
-    private float turnSpeed = 35.0f;
-    private float horizontalInput;
-    private float forwardInput;
+    [SerializeField] float speed = 20.0f;
+    [SerializeField] readonly float turnSpeed = 35.0f;
+    [SerializeField] float horizontalInput;
+    [SerializeField] float forwardInput;
 
     public Camera mainCamera;
     public Camera secondCamera;
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
 
     public string inputID;
     
-    void Update()
+    void FixedUpdate()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
@@ -26,8 +26,10 @@ public class PlayerController : MonoBehaviour
         
         horizontalInput = Input.GetAxis("Horizontal" + inputID);
         forwardInput = Input.GetAxis("Vertical" + inputID);
+
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
         
